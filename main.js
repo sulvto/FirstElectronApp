240fae92cac35f166021620cd2aca281d1414086
sulvto <lpetrel@outlook.com>
2016-03-07 13:45:15 +0800

    find module ‘jquery’

--- main.js
+++ main.js
@@ -6,34 +6,37 @@ const BrowserWindow = electron.BrowserWindow;  // Module to create native browse
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
- var mainWindow = null;
+var mainWindow = null;
+var a = "qinchao";
 
- // Quit when all windows are closed.
- app.on('window-all-closed', function() {
-   // On OS X it is common for applications and their menu bar
-     // to stay active until the user quits explicitly with Cmd + Q
-      if (process.platform != 'darwin') {
-           app.quit();
-             }
-             });
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
 
-             // This method will be called when Electron has finished
-             // initialization and is ready to create browser windows.
-             app.on('ready', function() {
-               // Create the browser window.
-                 mainWindow = new BrowserWindow({width: 800, height: 600});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function () {
 
-                   // and load the index.html of the app.
-                     mainWindow.loadURL('file://' + __dirname + '/index.html');
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
 
-                       // Open the DevTools.
-                         mainWindow.webContents.openDevTools();
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    //mainWindow.loadRUL('http://www.douban.com/people/slzw/');
 
-                           // Emitted when the window is closed.
-                             mainWindow.on('closed', function() {
-                                 // Dereference the window object, usually you would store windows
-                                     // in an array if your app supports multi windows, this is the time
-                                         // when you should delete the corresponding element.
-                                             mainWindow = null;
-                                               });
-                                               });
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,43 @@
 /**
  * Created by sulvto on 16-3-7.
  */
-alert("hello");
+
+var app = angular.module('mainApp', []);
+console.log(app);
+
+//app .config(['$routeProvider',function ($routeProvider) {
+//    $routeProvider
+//        .when('/nav', {
+//            templateUrl: 'list.html',
+//            controller: 'RouteListCtl'
+//        })
+//        .when('/list/:id', {
+//            templateUrl: 'detail.html',
+//            controller: 'RouteDetailCtl'
+//        })
+//        .otherwise({
+//            redirectTo: '/list'
+//        });
+//}]);
+
+app.directive('mainNav', function () {
+
+    return {
+        restrict: 'A',
+        templateUrl: 'static/view/nav.html',
+        replace: true
+    }
+});
+
+app.directive('mainBody', function () {
+    return {
+        restrict: 'A',
+        templateUrl: 'static/view/body.html',
+        replace: true
+    }
+});
+
+//controller
+app.controller('mainCtl', function ($scope) {
+    console.log("RUN mainCtl");
+});
