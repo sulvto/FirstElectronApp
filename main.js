ff70fb8e2d3fbb7bc2e77bb88ba2748e4676f856
sulvto <lpetrel@outlook.com>
2016-04-21 23:49:12 +0800

    webview ipc call

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.on('window-all-closed', function () {
 app.on('ready', function () {
 
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 800});
     mainWindow.setProgressBar(0.5);
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
--- static/js/guestPage.js
+++ static/js/guestPage.js
@@ -4,16 +4,20 @@
 ipc = require('electron').ipcRenderer;
 var $ = require('./jquery.min.js');
 
-ipc.on('$', function (e,msg) {
+ipc.on('$', function (e, msg) {
     console.log(msg);
     var $s = $(msg.selector);
     console.log($s);
     for (var i = 0, len = msg.fns.length; i < len; i++) {
         var fn = msg.fns[i];
-        console.log(fn.name+""  + fn.value);
-        if(fn.value) {
-            $s = $s[fn.name](fn.value);
-        }else{
+        console.log(fn.name + "" + fn.value);
+        if (fn.value) {
+            if (fn.value instanceof Array) {
+                $s = $s[fn.name](fn.value[0], fn.value[1]);
+            } else {
+                $s = $s[fn.name](fn.value);
+            }
+        } else {
             $s = $s[fn.name]();
         }
     }
